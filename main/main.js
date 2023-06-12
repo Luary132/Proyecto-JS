bc25be392e669e27cde335e07f0313f0bedc4af6
Luary132 <iangabferro@hotmail.com>
2023-06-11 21:54:17 -0300

    0.27

--- main/main.js
+++ main/main.js
@@ -4,14 +4,39 @@ $(document).ready(function(){
         mode: 'fade',
         captions: true,
         slideWidth: 1400,
-      });   
+        });
+      });
+      
+
+    /*post array/json */
+    articles.forEach((item, index)=> {
+      var post = ` 
+      <article class="post">
+      <h2>${item.title}</h2>
+      <span>${item.date}</span>
+      <button class="coment">comentar</button>
+      <p>
+        ${item.content}
+      </p>
+      <a href="#" class="button-more">Leer mas</a>
+      </article>
+      `;
+      
+  $('#post').append(post)
+
+
     });
 
 
-  console.log(articles)
-  
 
+  });
+  /*<article class="post">
+    <h2>${item.title}</h2>
+    <span>${item.date}</span>
+    <button class="coment">comentar</button>
+    <p>
+      ${item.content}
+    </p>
+    <a href="#" class="button-more">Leer mas</a>
 
-
-    
-})
+</article>*/
